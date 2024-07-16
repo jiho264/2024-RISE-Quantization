16a793f049cb794aec0c8236363765e0979b3520
lee-LAB <jiho264@naver.com>
2024-07-16 15:40:20 +0900

    Fix running sh file of AdaRound

--- run_myAdaRound.sh
+++ run_myAdaRound.sh
@@ -10,7 +10,7 @@ for _i in "${BASES[@]}"; do
         elif [ "$_lr" == "0.001" ]; then
             lr_name="1e-3"
         fi
-        FILENAME="logs/${SCHEMES}_${_i}_CH_W${W_bit}A32_LR${lr_name}.log"
+        FILENAME="logs/${SCHEMES}_${_i}_CH_W4A32_LR${lr_name}.log"
         echo "Running test case for ${FILENAME}"
         python main_myAdaRound.py --scheme $SCHEMES $PER_CHANNEL_FLAG --lr ${_lr} --BaseScheme $_i | tee ${FILENAME}
         echo ""
